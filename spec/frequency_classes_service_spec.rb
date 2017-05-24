430732be80e99812a762cc01100389097099fa95
Jan Zaydowicz <jan.zaydowicz@googlemail.com>
2017-05-24 12:47:27 +0200

    Remove init test
    as the test becomes unnecessary

--- spec/frequency_classes_service_spec.rb
+++ spec/frequency_classes_service_spec.rb
@@ -4,14 +4,6 @@ describe 'FrequencyClassesService' do
   let(:frequency_classes_service) { FrequencyClassesService.new }
   let(:sample_word) { 'haben' }
 
-  describe '#initialize' do
-    it 'should return a FrequencyClassesService instance' do
-      expect(frequency_classes_service).to be_instance_of(
-        FrequencyClassesService
-      )
-    end
-  end
-
   describe '#calculate' do
     it 'should calculate the frequency class for haben to match 3' do
       expect(frequency_classes_service.calculate(sample_word)).to eq 3
