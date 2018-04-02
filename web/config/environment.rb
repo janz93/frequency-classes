b2ac0f75dbaec1cff95246eba7777c7df69b5931
Jan Zaydowicz <jan.zaydowicz@wlw.de>
2018-04-02 20:18:18 +0200

    Update hanami to the latest version - fix structure issues

--- web/config/environment.rb
+++ web/config/environment.rb
@@ -1,7 +1,7 @@
 require 'bundler/setup'
 require 'hanami/setup'
 require 'hanami/model'
-require_relative '../lib/bookshelf'
+require_relative '../lib/frequency_classes'
 require_relative '../apps/web/application'
 
 Hanami.configure do
@@ -14,9 +14,9 @@ Hanami.configure do
     # Available options:
     #
     #  * SQL adapter
-    #    adapter :sql, 'sqlite://db/bookshelf_development.sqlite3'
-    #    adapter :sql, 'postgresql://localhost/bookshelf_development'
-    #    adapter :sql, 'mysql://localhost/bookshelf_development'
+    #    adapter :sql, 'sqlite://db/frequency_classes_development.sqlite3'
+    #    adapter :sql, 'postgresql://localhost/frequency_classes_development'
+    #    adapter :sql, 'mysql://localhost/frequency_classes_development'
     #
     adapter :sql, ENV['DATABASE_URL']
 
@@ -28,7 +28,7 @@ Hanami.configure do
   end
 
   mailer do
-    root 'lib/bookshelf/mailers'
+    root 'lib/frequency_classes/mailers'
 
     # See http://hanamirb.org/guides/mailers/delivery
     delivery :test
@@ -40,7 +40,7 @@ Hanami.configure do
   end
 
   environment :production do
-    logger level: :info, formatter: :json
+    logger level: :info, formatter: :json, filter: []
 
     mailer do
       delivery :smtp, address: ENV['SMTP_HOST'], port: ENV['SMTP_PORT']
---
+++ web/lib/frequency_classes.rb
@@ -0,0 +1,2 @@
+module FrequencyClasses
+end
